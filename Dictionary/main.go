b1cc49ef75f9f03952a18f675e4738dc87c4aef4
Jerome <jerome.millot@histofig.com>
2020-05-10 17:18:32 +0200

    Cours Golang

--- Dictionary/main.go
+++ Dictionary/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"training.go/Dictionary/dictionary"
 )
 
 func main() {
-	dictionary.New("./badger")
+	d, err := dictionary.New("./badger")
+	handleError(err)
+	defer d.Close()
+
+}
+
+func handleError(err error) {
+	if err != nil {
+		fmt.Printf("Dictionary error:%v\n", err)
+		os.Exit(1)
+	}
 }
